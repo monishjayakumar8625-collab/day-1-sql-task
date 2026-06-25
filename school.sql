f913f480c753480bee5fe85127a227d936f32a12
monishjayakumar8625-collab <monishjayakumar8625@gmail.com>
2026-06-25 19:13:48 +0530

    Add files via upload

---
+++ school.sql
@@ -0,0 +1,11 @@
+-- create database schooldb;
+-- create table students(stud_id int , stud_name varchar(100) , stud_age int , stud_grade char(1));
+-- insert into students(stud_id,stud_name,stud_age,stud_grade) values (777 , 'Juu' , 19 , 'O');
+-- insert into students(stud_id,stud_name,stud_age,stud_grade) values (778 , 'Ash' , 19 , 'A');
+-- insert into students(stud_id,stud_name,stud_age,stud_grade) values (779 , 'Monu' , 19 , 'B');
+-- insert into students(stud_id,stud_name,stud_age,stud_grade) values (780 , 'Kishuu' , 19 , 'C');
+-- insert into students(stud_id,stud_name,stud_age,stud_grade) values (781 , 'Linx' , 19 , 'D');
+select * from students;
+
+
+drop table students;
